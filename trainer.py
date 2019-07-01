3c464a0bdefa7c18425a7855c28671866bbf82df
rohaldbUni <z5019999@ad.unsw.edu.au>
2019-07-01 14:15:32 +1000

    tensorboard done

--- trainer.py
+++ trainer.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-
+from torch.utils.tensorboard import SummaryWriter
 
 def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval, metrics=[],
         start_epoch=0):
@@ -13,14 +13,17 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
     Siamese network: Siamese loader, siamese model, contrastive loss
     Online triplet learning: batch loader, embedding model, online triplet loss
     """
+
+    writer = SummaryWriter(flush_secs=10)
+    writer_train_index = 0 #variable to store training index accross epochs
+
     for epoch in range(0, start_epoch):
         scheduler.step()
 
     for epoch in range(start_epoch, n_epochs):
         scheduler.step()
-
         # Train stage
-        train_loss, metrics = train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, metrics)
+        train_loss, metrics, writer_train_index = train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, metrics, writer, writer_train_index)
 
         message = 'Epoch: {}/{}. Train set: Average loss: {:.4f}'.format(epoch + 1, n_epochs, train_loss)
         for metric in metrics:
@@ -29,6 +32,8 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
         val_loss, metrics = test_epoch(val_loader, model, loss_fn, cuda, metrics)
         val_loss /= len(val_loader)
 
+        writer.add_scalar("eval_loss", val_loss, epoch)
+
         message += '\nEpoch: {}/{}. Validation set: Average loss: {:.4f}'.format(epoch + 1, n_epochs,
                                                                                  val_loss)
         for metric in metrics:
@@ -36,8 +41,11 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
 
         print(message)
 
+    writer.close()
 
-def train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, metrics):
+
+
+def train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, metrics, writer, writer_train_index):
     for metric in metrics:
         metric.reset()
 
@@ -60,22 +68,28 @@ def train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, met
         loss.backward()
         optimizer.step()
 
+
         for metric in metrics:
             metric(outputs, target, loss_outputs)
 
         if batch_idx % log_interval == 0:
+            writer_train_index += log_interval
+            writer.add_scalar("train_loss", np.mean(losses), writer_train_index)
+
             message = 'Train: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 batch_idx * len(triplet[0]), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), np.mean(losses))
+
             for metric in metrics:
                 message += '\t{}: {}'.format(metric.name(), metric.value())
 
             print(message)
             losses = []
 
-
     total_loss /= (batch_idx + 1)
-    return total_loss, metrics
+    writer.close()
+
+    return total_loss, metrics, writer_train_index
 
 
 def test_epoch(val_loader, model, loss_fn, cuda, metrics):
