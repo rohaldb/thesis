c1cdccac7e8cae73692808119aa227f759f76ddf
rohaldbUni <z5019999@ad.unsw.edu.au>
2019-07-03 19:59:54 +1000

    shit balanced batch sampler made, still need to implement FunctionNegativeTripletSelector

--- datasets.py
+++ datasets.py
@@ -58,42 +58,87 @@ class TripletAudio(Dataset):
         return self.train_KNN.shape[0] if self.train else self.test_KNN.shape[0]
 
 
+class AudioTrainDataset(Dataset):
+    """
+    Training dataset
+    """
+
+    def __init__(self, K):
+        #comibne close and far neg indicies
+        self.data = torch.from_numpy(np.loadtxt('data/trainData.txt', dtype=np.float32))
+        self.KNN = pd.read_csv('data/trainKNN.csv', index_col=0)
+        self.K = K
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.KNN.shape[0]
+
+class AudioTestDataset(Dataset):
+    """
+    Training dataset
+    """
+
+    def __init__(self, K):
+        #comibne close and far neg indicies
+        self.data = torch.from_numpy(np.loadtxt('data/valData.txt', dtype=np.float32))
+        self.KNN = pd.read_csv('data/valKNN.csv', index_col=0)
+        self.K = K
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.KNN.shape[0]
+
+
 
 class BalancedBatchSampler(BatchSampler):
     """
-    BatchSampler - from a MNIST-like dataset, samples n_classes and within these classes samples n_samples.
+    BatchSampler - from a dataset, samples n_classes and within these classes samples n_samples.
     Returns batches of size n_classes * n_samples
     """
 
-    def __init__(self, labels, n_classes, n_samples):
-        self.labels = labels
-        self.labels_set = list(set(self.labels.numpy()))
-        self.label_to_indices = {label: np.where(self.labels.numpy() == label)[0]
-                                 for label in self.labels_set}
-        for l in self.labels_set:
-            np.random.shuffle(self.label_to_indices[l])
-        self.used_label_indices_count = {label: 0 for label in self.labels_set}
-        self.count = 0
-        self.n_classes = n_classes
-        self.n_samples = n_samples
-        self.n_dataset = len(self.labels)
-        self.batch_size = self.n_samples * self.n_classes
+    def __init__(self, dataset):
+
+        self.dataset = dataset
+        self.n_anchors = 10
+        self.n_pos = 5
+        self.n_close_neg = 10
+        self.n_far_neg = 10
+
+        self.batch_size = self.n_anchors * (self.n_pos + self.n_close_neg + self.n_far_neg)
+        self.anchor_indicies = np.copy(self.KNN.index.values)
+        np.random.shuffle(self.anchor_indicies) #shuffle so we get random anchors
+        self.n_dataset = self.KNN.shape[0]
+        self.used_anchors = 0 #counter to move along the anchor indicies list
 
     def __iter__(self):
-        self.count = 0
-        while self.count + self.batch_size < self.n_dataset:
-            classes = np.random.choice(self.labels_set, self.n_classes, replace=False)
-            indices = []
-            for class_ in classes:
-                indices.extend(self.label_to_indices[class_][
-                               self.used_label_indices_count[class_]:self.used_label_indices_count[
-                                                                         class_] + self.n_samples])
-                self.used_label_indices_count[class_] += self.n_samples
-                if self.used_label_indices_count[class_] + self.n_samples > len(self.label_to_indices[class_]):
-                    np.random.shuffle(self.label_to_indices[class_])
-                    self.used_label_indices_count[class_] = 0
-            yield indices
-            self.count += self.n_classes * self.n_samples
+
+        self.used_anchors = 0
+
+
+        while self.used_anchors + self.batch_size < self.n_dataset:
+            batch = set() #set to ensure no duplicates
+            # find new anchor points
+            batch_anchor_indicies = self.anchor_indicies[self.used_anchors:self.used_anchors + self.n_anchors]
+            self.used_anchors += self.n_anchors
+            batch.update(batch_anchor_indicies)
+            # add some pos and neg points to the batch
+            for anchor_index in batch_anchor_indicies:
+                row = self.KNN.iloc[anchor_index]
+                batch.update(np.random.choice(row[0:self.K], self.n_pos, replace=False)) #add pos
+                batch.update(np.random.choice(row[self.K:], self.n_close_neg + self.n_far_neg, replace=False)) #add neg
+
+            #may have added duplicates in process. If so just fill indicies w random
+            while len(batch) < self.batch_size:
+                batch.add(self.anchor_indicies[self.used_anchors])
+                self.used_anchors += 1
+
+            yield list(batch)
+
+        print(self.used_anchors, self.batch_size , self.n_dataset)
 
     def __len__(self):
         return self.n_dataset // self.batch_size
