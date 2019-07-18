e89ba0fa53e79899e2dccd92d7e0c88b743835df
rohaldbUni <z5019999@ad.unsw.edu.au>
2019-07-18 10:58:24 +1000

    Finished tests for recall measure and removed prints

--- recall.py
+++ recall.py
@@ -14,14 +14,11 @@ class Recall:
 
         self.outputs = self.model(self.data).numpy()
 
-        print('outputs', self.outputs)
         self.code_len = self.outputs.shape[1]
         quantized_outputs = np.heaviside(self.outputs, 0).astype(int)
-        print('quantized_outputs', quantized_outputs)
         self.bucket_hash = BucketHash()
         for i, x in enumerate(quantized_outputs):
             self.bucket_hash.add(x, i) #hash[bucket].append(i)
-        print('bucket hash', self.bucket_hash.hash)
 
         for q in query:
             items = self.top_k_items(q)
@@ -35,22 +32,17 @@ class Recall:
         cand = [] #candidate set
         proj = self.model(query.unsqueeze(0)).squeeze().numpy()
         code = np.heaviside(proj, 0).astype(int)
-        print('proj', proj, 'code', code)
         i = 0
         sorted_proj, self.proj_map = self.sorted_to_proj_map(proj)
 
         while len(cand) < self.num_cand and i < 2**self.code_len:
             b = self.generate_bucket(code, sorted_proj, i)
             #need quick method to go from bucket to entry in bucket
-            print('bucket returned', b)
             items = self.bucket_hash.get(b)
             cand.extend(items)
             i += 1
-            print('-----')
 
-        print('cand', cand)
-        cand = sorted(cand, key=lambda x: np.linalg.norm(self.data[x]-query), reverse = True)
-        print('sorted cand', cand)
+        cand = sorted(cand, key=lambda x: np.linalg.norm(self.data[x]-query))
         return cand[0:self.K]
 
     #distance of x from y
@@ -66,23 +58,21 @@ class Recall:
             sorted_flip = np.zeros(self.code_len, dtype=int)
         else:
             dist, sorted_flip  = heappop(self.heap)
-            print('popped', dist, sorted_flip)
             j = np.where(sorted_flip == 1)[0][-1]
             if j < self.code_len-1:
                 sorted_flip_plus = np.copy(sorted_flip)
                 sorted_flip_plus[j+1] = 1
                 to_add = (self.dist(sorted_proj, sorted_flip) + sorted_proj[j+1], sorted_flip_plus)
                 heappush(self.heap, to_add)
-                print('pushed', to_add)
                 sorted_flip_minus = np.copy(sorted_flip)
                 sorted_flip_minus[j] = 0
                 sorted_flip_minus[j+1] = 1
                 to_add = (self.dist(sorted_proj, sorted_flip) + sorted_proj[j+1] - sorted_proj[j], sorted_flip_minus)
                 heappush(self.heap, to_add)
-                print('pushed', to_add)
 
         return self.sorted_flip_to_bucket(sorted_flip, code)
 
+    #sorts the projection and returns map to original indicies
     def sorted_to_proj_map(self, proj):
         indicies = list(range(0, proj.shape[0]))
         sorted_proj, indicies = zip(*[(p,q) for p,q in sorted(zip(abs(proj),indicies))])
@@ -92,11 +82,9 @@ class Recall:
     def sorted_flip_to_bucket(self, sorted_flip, code):
         bucket = np.copy(code)
         i = 0
-        print(self.proj_map)
         while i < self.code_len:
             if sorted_flip[i] == 1:
                 l = self.proj_map[i]
-                print(i, l)
                 bucket[l] = 1 - code[l]
             i += 1
         return bucket
