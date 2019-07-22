9d5223aaefb409844f4e1321f864046df486e863
rohaldbUni <z5019999@ad.unsw.edu.au>
2019-07-23 09:47:04 +1000

    Updated the offline dataset to include softer negatives on occasion. Does so using the prob param to the function. Made small adjustments to testing and runner but nothing major.

--- datasets.py
+++ datasets.py
@@ -16,21 +16,37 @@ class TripletAudio(Dataset):
     def __init__(self, train, K, MAX_CLOSE_NEG, MAX_FAR_NEG):
         #comibne close and far neg indicies
         self.K = K
-        self.neg_indicies = list(range(K+1,K + MAX_CLOSE_NEG)) + list(range(-MAX_FAR_NEG,-1))
+        #note randint is inclusive of params
+        self.neg_indicies = np.concatenate((np.arange(K,K + MAX_CLOSE_NEG), np.arange(-MAX_FAR_NEG,0)))
         self.train = train
 
         if self.train:
             self.train_data = torch.from_numpy(np.loadtxt('data/trainData.txt', dtype=np.float32))
-            self.train_KNN = pd.read_csv('data/trainKNN.csv', index_col=0)
+            self.train_knn = pd.read_csv('data/trainKNN.csv', index_col=0)
         else:
             self.test_data = torch.from_numpy(np.loadtxt('data/valData.txt', dtype=np.float32))
-            self.test_KNN = pd.read_csv('data/valKNN.csv', index_col=0)
+            self.test_knn = pd.read_csv('data/valKNN.csv', index_col=0)
             #generate fixed trainin examples (indicies)
             self.test_triplet_indicies = [[
                     index,
-                    self.test_KNN.iloc[index][randint(0, K)], #pos
-                    self.test_KNN.iloc[index][np.random.choice(self.neg_indicies)] #neg
+                    self.test_knn.iloc[index][randint(0, K-1)], #pos
+                    self.weighted_neg_sampler(index, train=False)
                 ] for index in range(0, self.test_data.shape[0])]
+
+    #picks a strong negative (< MAX_CLOSE_NEG or > MAX_FAR_NEG) datapoint with prob p_strong_neg
+    def weighted_neg_sampler(self, row_index, train, p_strong_neg = 0.9):
+        knn = self.train_knn if train else self.test_knn
+        data = self.train_data if train else self.test_data
+
+        if np.random.choice(np.arange(0,2), p=[1 - p_strong_neg, p_strong_neg]):
+            return knn.iloc[row_index][np.random.choice(self.neg_indicies)] #strong neg
+        else:
+            # weak neg
+            neg_index = randint(0, data.shape[0]-1)
+            while neg_index in knn.iloc[row_index].values or neg_index == row_index:
+                neg_index = randint(0, data.shape[0]-1)
+            return neg_index
+
 
     def get_dataset(self):
         if self.train:
@@ -38,30 +54,32 @@ class TripletAudio(Dataset):
         else:
             return self.test_data
 
-    def get_KNN(self):
+    def get_knn(self):
         if self.train:
-            return self.train_KNN
+            return self.train_knn
         else:
-            return self.test_KNN
+            return self.test_knn
 
     def __getitem__(self, index):
         if self.train:
             anchor = self.train_data[index]
-            pos = self.train_data[self.train_KNN.iloc[index][randint(0, self.K)]]
-            neg = self.train_data[self.train_KNN.iloc[index][np.random.choice(self.neg_indicies)]]
+            pos_index = self.train_knn.iloc[index][randint(0, self.K-1)]
+            pos = self.train_data[pos_index]
+            neg_index = self.weighted_neg_sampler(index, True)
+            neg = self.train_data[neg_index]
         else:
             anchor = self.test_data[self.test_triplet_indicies[index][0]]
             pos = self.test_data[self.test_triplet_indicies[index][1]]
             neg = self.test_data[self.test_triplet_indicies[index][2]]
 
         # ensure the pos is closer to the point than the neg
-        assert( ((anchor-pos)**2).sum() - ((anchor-neg)**2).sum() < 0 )
+        assert((np.linalg.norm(anchor-pos) - np.linalg.norm(anchor-neg)) < 0)
 
         return (anchor.reshape(-1, 1), pos.reshape(-1, 1), neg.reshape(-1, 1)), [], index
 
 
     def __len__(self):
-        return self.train_KNN.shape[0] if self.train else self.test_KNN.shape[0]
+        return self.train_knn.shape[0] if self.train else self.test_knn.shape[0]
 
 
 class AudioTrainDataset(Dataset):
